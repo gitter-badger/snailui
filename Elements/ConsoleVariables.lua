051a967bfd66bbcc0e108791b0d9ce060b8a5d32
Snail <snail@heeroik.com>
2014-04-12 04:37:55 -0400

    Removed the addon source code

--- Elements/ConsoleVariables.lua
+++
@@ -1,9 +0,0 @@
--- ConsoleVariables.lua
--- Written by Snail
-
-function HandleConsoleVariables()
-	SetCVar("consolidateBuffs", "0")
-	SetCVar("ShowClassColorInNameplate", "1")
-	SetCVar("showArenaEnemyFrames", "0")
-	SetCVar("useUiScale", "0")
-end
--- Elements/Focus.lua
+++
@@ -1,9 +0,0 @@
--- Focus.lua
--- Written by Snail
-
-function HandleFocus(Self)
-	if GetConfiguration().Focus then
-		Focus = Self:Spawn("Focus")		
-		Focus:SetPoint(GetConfiguration().Focus.Anchor, GetConfiguration().Focus.X, GetConfiguration().Focus.Y)
-	end
-end
